7972165db44563c680c8dcaec8c7707e74257ce6
lo98be <bertol.lollo@gmail.com>
2017-03-08 15:53:33 +0100

    Second version, with internal double 7 segments driver

--- mplab_files.X/main.c
+++ mplab_files.X/main.c
@@ -1,63 +1,77 @@
 /* File:   newmain.c
  * Author: Lorenzo Bertolino
- *
  * Created on 20 december 2016, 9:31
  */
 
-#include<xc.h>
-/*
-#include<stdio.h>
-#include<stdlib.h>
-#define BtBp "%c%c%c%c%c%c%c%c"
-#define BtB(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
-*/
-__CONFIG (FOSC_XT & WDTE_OFF & PWRTE_OFF & CP_OFF & BOREN_ON & LVP_ON & CPD_OFF & WRT_ON);
+#include<htc.h>
+
+__CONFIG(FOSC_XT & WDTE_OFF & PWRTE_OFF & CP_OFF & BOREN_ON & LVP_ON & CPD_OFF & WRT_ON);
 #define _XTAL_FREQ 4000000
 
 
 //inizializza uC
 void Init(){
-	/* per prova:
-	return;
-	//*/
-	
-	//* impostazioni reali
 	//Port Configuration
 	TRISA=0b11111111;
 	TRISB=0;
+    TRISC=0;
+    OPTION_REG|=0b10000000;
 	//Convertitore on, Fosc/32
 	ADCON0=0b10000001;
 	//allineamento a sinistra e solo RA0 analogico, con Vref interna
-	ADCON1=0b01001110;//*/
+	ADCON1=0b00001110;//*/
 }
 
 //leggi da AN0
 unsigned int AN0Read(){
 	/* per prova
-	return 256;
-	//*/
-	
+	return 256;//*/
 	//* impostazioni reali
 	__delay_ms(1);
 	GO_DONE=1;//convertitore on su canale 0
-	__delay_ms(1);
+	while(GO_DONE);
 	return ADRESH;//*/
 }
-unsigned int decVal;
+
+unsigned int SevenSeg(unsigned int value){
+    switch(value){
+        case 0:
+            return 0b10111111;
+        case 1:
+            return 0b00000110;
+        case 2:
+            return 0b11011011;
+        case 3:
+            return 0b11001111;
+        case 4:
+            return 0b01100110;
+        case 5:
+            return 0b11101101;
+        case 6:
+            return 0b11111100;
+        case 7:
+            return 0b10000111;
+        case 8:
+            return 0b11111111;
+        case 9:
+            return 0b01100111;
+    }
+}
+
+
 int main() {
-	Init();
-	while(1){
-		decVal=(AN0Read()*80)/256;//richiederà un po'di cicli
-		PORTB=(decVal%10)<<4|((decVal/10)%10);//anche questo
-	//printf(""BtBp"\n",BtB(portb));	//debug
-	}
-    return 0;
+    Init();
+    while(1){
+        unsigned int decVal=(AN0Read()*80)/256;
+        unsigned int unita=decVal%10;
+        unsigned int decine=(decVal/10)%10;
+        
+        PORTC=SevenSeg (unita);
+        
+        if(decine)
+            PORTB=SevenSeg (decine);
+        else
+            PORTB=0;
+        __delay_ms(498);
+    }
 }
