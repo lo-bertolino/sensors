244bf059c4a9f1e0c31ba70308edf97935a64e12
Unknown <bertol.lollo@gmail.com>
2017-06-16 16:20:26 +0200

    nRF strutturato

--- mplab_files.X/main.c
+++ mplab_files.X/main.c
@@ -15,41 +15,55 @@
 #pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
 #pragma config EBTRB = OFF
 
-// Use project enums instead of #define for ON and OFF.
 #include <htc.h>
 #include <stdlib.h>
 #include "spi.h"
 #include "nRF24L01.h"
 #include "wl_module.h"
+
 #define _XTAL_FREQ 16000000
 #define DHTPin PORTCbits.RC0
 #define DHTPinDir TRISCbits.TRISC0
+#define UID 1 //ID sensore
 
-unsigned char humid, res, time_out;
-char temp;
+//nRF
+#define nRF_use
 unsigned char payload[wl_module_PAYLOAD];
 
+struct {
+	unsigned char ID;
+	short temp;
+	unsigned short humid;
+	unsigned short counter;
+} dati;
+
 //dht
-char readDHT (){
+//#define DHT_use
+char time_out, res;
+unsigned short humid;
+short temp;
+
+char readDHT()
+{
 	unsigned char i, k, dat = 0; // k is used to count 1 bit reading duration
-	if(!time_out) return 0;
-	for(i = 0; i<8; i++){
+	if (!time_out) return 0;
+	for (i = 0; i < 8; i++) {
 		k = 0;
-		while(!DHTPin){ // Wait until pin goes high
+		while (!DHTPin) { // Wait until pin goes high
 			k++;
-			if(k>100){
+			if (k > 100) {
 				time_out = 0;
 				return 0;
 			}
 			__delay_us(1);
 		}
 		__delay_us(30);
-		if(!DHTPin) dat &= 0<<(7-i); // Clear bit (7 - i)
-		else{
-			dat |= 1<<(7-i); // Set bit (7 - i)
-			while(DHTPin){ // Wait until pin goes low
+		if (!DHTPin) dat &= 0 << (7 - i); // Clear bit (7 - i)
+		else {
+			dat |= 1 << (7 - i); // Set bit (7 - i)
+			while (DHTPin) { // Wait until pin goes low
 				k++;
-				if(k>100){
+				if (k > 100) {
 					time_out = 0;
 					return 0;
 				}
@@ -60,7 +74,8 @@ char readDHT (){
 	return dat;
 }
 
-char beginDHT (){
+char beginDHT()
+{
 	DHTPinDir = 0; // Configure connection pin as output
 	DHTPin &= 0; // Connection pin output low
 	__delay_ms(25);
@@ -68,9 +83,9 @@ char beginDHT (){
 	__delay_us(30);
 	DHTPinDir = 1; // Configure connection pin as input
 	__delay_us(40);
-	if(!DHTPin){ // Read and test if connection pin is low
+	if (!DHTPin) { // Read and test if connection pin is low
 		__delay_us(80);
-		if(DHTPin){ // Read and test if connection pin is high
+		if (DHTPin) { // Read and test if connection pin is high
 			__delay_us(50);
 			return 1;
 		}
@@ -78,32 +93,34 @@ char beginDHT (){
 	return 0;
 }
 
-void DHTHandler (){
+void DHTHandler()
+{
 	unsigned char t_byte1, t_byte2, rh_byte1, rh_byte2, checkSum;
 	time_out = 1;
-	if(!beginDHT())
+	if (!beginDHT())
 		res = 'r'; // errore risposta assente
 	rh_byte1 = readDHT();
 	rh_byte2 = readDHT();
 	t_byte1 = readDHT();
 	t_byte2 = readDHT();
 	checkSum = readDHT();
-	if(time_out)
+	if (time_out)
 		res = 't'; //errore di timeout
-	if(checkSum!=((rh_byte1+rh_byte2+t_byte1+t_byte2) & 0xFF))
+	if (checkSum != ((rh_byte1 + rh_byte2 + t_byte1 + t_byte2) & 0xFF))
 		res = 'c'; //errore di checksum
 	humid = rh_byte1;
-	humid = (humid<<8)|rh_byte2;
+	humid = (humid << 8) | rh_byte2;
 	temp = t_byte1;
-	temp = (temp<<8)|t_byte2;
-	if(temp>0X80)
-		temp -= temp&0X7F; //rendi  negativo
+	temp = (temp << 8) | t_byte2;
+	if (temp > 0X80)
+		temp -= temp & 0X7F; //rendi  negativo
 	res = 's'; //successo
 }
 
 //inizializza uC
 
-void Init (){
+void Init()
+{
 	//debug
 	TRISB = 0;
 	//nRF setup
@@ -112,17 +129,29 @@ void Init (){
 	//interrupts
 	INTCONbits.PEIE = 1;
 	INTCONbits.GIE = 1;
+	//reset dati
+	dati.ID = UID; //id
+	dati.counter = 0;
 }
 
-int main(){
+int main()
+{
 	Init();
-	while(1){
-		wl_module_send(payload, wl_module_PAYLOAD);
+	while (1) {
+#ifdef DHT_use
 		DHTHandler();
-		if(res=='s') PORTB = temp;
-		else if(res=='r') PORTB = 0xf1;
-		else if(res=='t') PORTB = 0xf2;
-		else if(res=='c') PORTB = 0xf3;
+		if (res == 's') PORTB = temp;
+		else if (res == 'r') PORTB = 0xf1;
+		else if (res == 't') PORTB = 0xf2;
+		else if (res == 'c') PORTB = 0xf3;
+#endif
+#ifdef nRF_use
+		dati.humid = 80; //solo per test
+		dati.temp = 25; //solo per test
+		dati.counter++; //questo no 
+		payload = dati;
+		wl_module_send(payload, wl_module_PAYLOAD);
+#endif
 		__delay_ms(2000);
 	}
 }
